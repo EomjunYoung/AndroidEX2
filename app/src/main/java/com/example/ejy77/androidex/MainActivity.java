6b22258d2e4e0434b72f031bda15c1902b770c59
EomjunYoung <theoneday737@naver.com>
2017-12-27 18:16:34 +0900

    modify final

--- app/src/main/java/com/example/ejy77/androidex/MainActivity.java
+++ app/src/main/java/com/example/ejy77/androidex/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.ejy77.androidex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(getApplicationContext(), "테스트", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "테스트2", Toast.LENGTH_SHORT).show();
-        Toast.makeText(getApplicationContext(), "테스트3", Toast.LENGTH_SHORT).show();
+        btn = (Button)findViewById(R.id.btn);
+        
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "테스트", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
     }
