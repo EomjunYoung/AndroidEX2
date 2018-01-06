28dd1c548c4eeedac4ed8b57c8ce044ec36b4d05
EomjunYoung <theoneday737@naver.com>
2018-01-06 18:43:38 +0900

    test

--- app/src/main/java/com/example/ejy77/androidex/MainActivity.java
+++ app/src/main/java/com/example/ejy77/androidex/MainActivity.java
@@ -4,30 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+
+    TextView ksyeon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button)findViewById(R.id.btn);
-        
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ksyeon=(TextView)findViewById(R.id.ksyeon);
 
-                Toast.makeText(getApplicationContext(), "테스트", Toast.LENGTH_SHORT).show();
 
-            }
-        });
+
 
 
     }
